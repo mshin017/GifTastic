5f0dd4e331f67eeaf8727f219ded2355fb527ff4
Michelle Shin <mshin017@ucr.edu>
2018-05-22 00:10:05 -0700

    updating js

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,4 +1,4 @@
-var topics = ["Dog","Cat","Squirel","Monkey","Elephant","Giraffe","Lion","Tiger",
+var topics = ["Dog","Cat","Squirrel","Monkey","Elephant","Giraffe","Lion","Tiger",
 "Wolf","Bear","Panda","Fox", "Red Panda",];
 var extraTopics = [];
 
@@ -29,7 +29,7 @@ $(document).on("click", ".topic-button", displayGifs);
 
 
 function displayGifs() {
-    var animal = $(this).attr("data-animal");
+    var animal = $(this).text();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 console.log(queryURL);
     $.ajax({
@@ -46,11 +46,11 @@ console.log(queryURL);
           var p = $("<p>").text("Rating: " + rating);
           var animalImage = $("<img>");
 
-          animalImage.attr("src", results[i].images.fixed_height.url);
-          animalImage.attr("gif-still", results[i].images.fixed_height.url);
+          animalImage.attr("src", results[i].images.fixed_height_still.url);
+          animalImage.attr("gif-still", results[i].images.fixed_height_still.url);
           animalImage.attr("gif-animate", results[i].images.fixed_height.url);
           animalImage.attr("gif-state", "still")
-          animalImage.attr("image-gif");
+          animalImage.addClass("gif");
 
           gifDiv.append(p);
           gifDiv.prepend(animalImage);
@@ -61,7 +61,7 @@ console.log(queryURL);
 
 
 function playGif() {
-    $("body").on("click", ".image-gif", function() {
+    $(".gif").on("click", function() {
         var state = $(this).attr("gif-state")
         console.log(state);
         if (state === "still") {
@@ -69,8 +69,8 @@ function playGif() {
             $(this).attr("gif-state", "animate");
         }
         else {
-        $(this).attr("src", $(this).attr("gif-still"));
-        $(this).attr("gif-state", "still");
+            $(this).attr("src", $(this).attr("gif-still"));
+            $(this).attr("gif-state", "still");
         }
         });
 }
